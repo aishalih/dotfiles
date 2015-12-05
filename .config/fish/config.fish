b0081f14ecc2661b54978d5d541a2cb77856895b
Blaisck <blaisck@gmail.com>
2015-12-05 12:57:14 +0700

    add fish conf

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH /home/bl/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG /home/bl/.config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
+# Alias
+alias tmux "env TERM=screen-256color-bce tmux"
+
+
